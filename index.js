2aa3caebf209f2426ce1530ac908c874ae1dba80
Grégory Paul <me@gpaul.me>
2023-12-03 10:06:29 +0100

    fix sw registration

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ data.forEach((item, idx) => createRow(item, idx));
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
+      const registration = await navigator.serviceWorker.register("./sw.js", {
         scope: "/",
       });
       if (registration.installing) {
